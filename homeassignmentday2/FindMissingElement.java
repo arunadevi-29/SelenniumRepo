1fee7c33739c2c88d2bed6a699ab47ca67ff0135
arunadevi-29 <ggarunadevitdr00@gmail.com>
2024-05-31 08:24:49 +0530

    Add files via upload
    week2day2

---
+++ homeassignmentday2/FindMissingElement.java
@@ -0,0 +1,22 @@
+package homeassignmentday2;
+
+import java.util.Arrays;
+
+public class FindMissingElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[]={1,4,3,2,8,6,7};
+		Arrays.sort(array);
+		int arraylen=array.length;
+		for (int i=0;i<arraylen;i++)
+		{
+			if(array[i]!=i+1) {
+				System.out.println("The missinig number is "+(i+1));
+				break;
+			}
+		}
+			
+	}
+
+}
