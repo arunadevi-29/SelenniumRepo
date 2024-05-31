5ff83e4451e57cc7f29e2e4a91ad3e675be8106f
arunadevi-29 <ggarunadevitdr00@gmail.com>
2024-05-31 08:23:29 +0530

    Add files via upload
    week1day1

---
+++ homeassignmentday1/Fibonacci.java
@@ -0,0 +1,20 @@
+package homeassignmentday1;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c,i,j;
+		a=0;
+		b=1;
+		j=8;
+		System.out.println(a+"\n"+b);
+		for(i=2;i<j;i++) {
+			c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+		}
+	}
+
+}
